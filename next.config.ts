c59ef619cbffe6c835ca51b5e875db8fd31dc2b6
mango <mango@mangodeMacBook-Pro.local>
2026-03-22 20:44:56 +0800

    修改nextjs配置

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["*"],
 };
 
 export default nextConfig;
